5554c756650cb4c0c6116f210170fd66f1e40670
Javier Peluso <jpelusocoderhouse@gmail.com>
2023-02-16 17:47:31 -0300

    update laps

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
+//VARIABLES
 let centesimas = 0;
 let segundos = 0;
 let minutos = 0;
 let horas = 0;
+let vueltas = 0
 
+//SELECTORES
+const contenedorProductos = document.querySelector('#contenedorProductos')
+
+
+//FUNCIONES
 const inicio = () => {
 	control = setInterval(cronometro,10);
 	document.getElementById("inicio").disabled = true;
 	document.getElementById("parar").disabled = false;
 	document.getElementById("continuar").disabled = true;
 	document.getElementById("reinicio").disabled = false;
+	document.getElementById("vuelta").disabled = false;
 }
 const parar = () =>{
 	clearInterval(control);
 	document.getElementById("parar").disabled = true;
 	document.getElementById("continuar").disabled = false;
+}
+
+const vuelta = () =>{
+	clearInterval(control)
+	document.getElementById("vuelta").disabled = true
+	vueltas++
+	const div = document.createElement('div')
+    div.classList.add('vuelta')
+    div.innerHTML = `
+    Vuelta ${vueltas}: ${horas}:${minutos}:${segundos}:${centesimas}`
+    contenedorProductos.append(div)
+	inicio()
 }
 const reinicio = () =>{
 	clearInterval(control);
@@ -21,10 +41,12 @@ const reinicio = () =>{
 	segundos = 0;
 	minutos = 0;
 	horas = 0;
+	vueltas = 0
 	Centesimas.innerHTML = ":00";
 	Segundos.innerHTML = ":00";
 	Minutos.innerHTML = ":00";
 	Horas.innerHTML = "00";
+	contenedorProductos.innerHTML = ""
 	document.getElementById("inicio").disabled = false;
 	document.getElementById("parar").disabled = true;
 	document.getElementById("continuar").disabled = true;
